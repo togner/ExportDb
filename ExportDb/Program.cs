4551c968763d6499fe33424fa2228f3a3666905e
togner <togner@gmail.com>
2012-05-01 20:05:26 -0400

    Added references to SMO DLLs (SQL Server 2008 R2).

--- ExportDb/Program.cs
+++ ExportDb/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace ExportDb
 {
-    class Program
+    /// <summary>
+    /// Where to get SMO DLLs:
+    /// http://msdn.microsoft.com/en-us/library/ms162129%28v=SQL.105%29.aspx
+    /// 
+    /// How to use SMOs:
+    /// http://msdn.microsoft.com/en-us/library/ms162202.aspx
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Arguments _arguments = new Arguments(args);
             try
@@ -14,6 +24,20 @@ namespace ExportDb
                     Program.PrintHelp();
                     return;
                 }
+
+                string _connectionString = "server=(local);database=Metadata;trusted_connection=yes";
+
+                SqlConnection _connection = new SqlConnection(_connectionString);
+                ServerConnection _serverConn = new ServerConnection(_connection);
+                Server _server = new Server(_serverConn);
+
+                Database _db = _server.Databases[_connection.Database];
+                foreach (Table _table in _db.Tables)
+                {
+                    Console.WriteLine(_table.Name);
+                }
+
+                Console.ReadKey();
             }
             catch (Exception _e)
             {
