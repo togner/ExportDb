249776a02a5912cfefd21c3af688245d3d16d932
togner <togner@gmail.com>
2012-05-08 20:02:07 -0400

    Implemented all main features - scripts data for all non-systems tables in DB.

--- ExportDb/Program.cs
+++ ExportDb/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
 using Microsoft.SqlServer.Management.Smo;
 
 namespace ExportDb
@@ -19,25 +23,69 @@ namespace ExportDb
             Arguments _arguments = new Arguments(args);
             try
             {
+                // params
                 if (_arguments["?"] != null)
                 {
                     Program.PrintHelp();
                     return;
                 }
-
                 string _connectionString = "server=(local);database=Metadata;trusted_connection=yes";
-
+                ScriptingOptions _options = new ScriptingOptions()
+                {
+                    AllowSystemObjects = false,
+                    ScriptData = true,
+                    ScriptSchema = false
+                };
+                string _outputDir = @"C:\data";
+                Encoding _encoding = Encoding.Unicode;
+                int _commandTerminatorInterval = 100;
+                
+                // connection
                 SqlConnection _connection = new SqlConnection(_connectionString);
                 ServerConnection _serverConn = new ServerConnection(_connection);
                 Server _server = new Server(_serverConn);
-
+                _server.SetDefaultInitFields(typeof(Table), "IsSystemObject");
                 Database _db = _server.Databases[_connection.Database];
+
+                Scripter _scripter = new Scripter(_server);
+                _scripter.Options = _options;
                 foreach (Table _table in _db.Tables)
                 {
-                    Console.WriteLine(_table.Name);
+                    if (!_table.IsSystemObject)
+                    {
+                        // output file
+                        string _regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+                        Regex _regex = new Regex(string.Format("[{0}]", Regex.Escape(_regexSearch)));
+                        string _escapedName = _regex.Replace(_table.Name, ".");
+                        string _fileName = Path.Combine(_outputDir, string.Format("{0}.{1}.Table.sql", _table.Schema, _escapedName));
+                        string _directoryName = Path.GetDirectoryName(_fileName);
+                        if (!Directory.Exists(_directoryName))
+                        {
+                            Directory.CreateDirectory(_directoryName);
+                        }
+
+                        // alternatively
+                        // _scripter.Options.ToFileOnly = true;
+                        // _scripter.Options.FileName = _fileName;
+                        // _options.NoCommandTerminator = true;
+                        // _scripter.EnumScript(new Urn[] { _table.Urn });
+                        using (StreamWriter _writer = new StreamWriter(_fileName, false, _encoding))
+                        {
+                            int _row = 0;
+                            foreach (string _scriptLine in _scripter.EnumScript(new Urn[] { _table.Urn }))
+                            {
+                                if (_row > 0 && _row % _commandTerminatorInterval == 0)
+                                {
+                                    _writer.WriteLine("GO");
+                                }
+                                _writer.WriteLine(_scriptLine);
+                                _row++;
+                            }
+                        }
+                    }
                 }
 
-                Console.ReadKey();
+                //Console.ReadKey();
             }
             catch (Exception _e)
             {
